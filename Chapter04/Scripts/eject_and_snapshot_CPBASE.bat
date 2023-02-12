aaffe5b87c23d41aa6a058aac410b0bb1efe8c29
higherintelligence <92738512+higherintelligence@users.noreply.github.com>
2023-02-12 15:49:52 -0500

    Update eject_and_snapshot_CPBASE.bat
    Change the name of the VM from CPBASE1 to CPBASE

--- Chapter04/Scripts/eject_and_snapshot_CPBASE.bat
+++ Chapter04/Scripts/eject_and_snapshot_CPBASE.bat
@@ -1,5 +1,4 @@
 SET PATH=%PATH%;C:\Program Files\Oracle\VirtualBox
-SET VM=CPBASE1
+SET VM=CPBASE
 VBoxManage storageattach %VM% --storagectl "IDE" --port 0 --device 0 --medium emptydrive
 VBoxManage snapshot %VM% take "Snapshot 1" --description "Do NOT delete!!! This snapshot is used to spawn the linked clones of all Check Point components in the lab."
-
