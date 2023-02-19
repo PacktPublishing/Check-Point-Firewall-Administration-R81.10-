0c514615f5464f610d60f9f6f025844aef642c67
higherintelligence <92738512+higherintelligence@users.noreply.github.com>
2023-02-19 17:25:14 -0500

    Update LeftHost.ps1
    1. Aligned script content with those used for other hosts. 
    2. Included a line for DNS configuration.

--- Chapter04/Scripts/LeftHost.ps1
+++ Chapter04/Scripts/LeftHost.ps1
@@ -1,7 +1,12 @@
+#Caution!!! This script disables the Windows Update Services.
+#It is intended for the use in isolated lab environments for space saving purposes only.
+#If you do not have space constraints, comment-out five lines before the last one and save the script!!! 
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
 Rename-Computer -NewName LeftHost
 New-NetIPAddress -IPAddress 10.10.10.10 -DefaultGateway 10.10.10.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
-Get-ScheduledTask -TaskName Scheduled Start | Disable-ScheduledTask  -Verbose
+Set-DNSClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses 10.20.20.10
+$WindowsUpdate = "Scheduled Start"
+Get-ScheduledTask -TaskName $WindowsUpdate | Disable-ScheduledTask  -Verbose
 Get-ScheduledTask -TaskName StartComponentCleanUp | Disable-ScheduledTask  -Verbose
 Set-Service wuauserv -Startup Disabled
 Stop-Service wuauserv -Force
