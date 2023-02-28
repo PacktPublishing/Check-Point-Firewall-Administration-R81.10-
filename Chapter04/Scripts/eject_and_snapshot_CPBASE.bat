0cf64bee37b514f3528ec5548502646065d4affc
higherintelligence <92738512+higherintelligence@users.noreply.github.com>
2023-02-28 16:29:37 -0500

    Update eject_and_snapshot_CPBASE.bat

--- Chapter04/Scripts/eject_and_snapshot_CPBASE.bat
+++ Chapter04/Scripts/eject_and_snapshot_CPBASE.bat
@@ -1,7 +1,7 @@
-REM "This script ejecting the media and creating snapshot of CPBASE VM on LabHost PC."
-echo Ejecting the media and creating snapshot of CPBASE VM on LabHost PC...
+@ECHO OFF
+REM "This script ejecting attached media and creating snapshot of CPBASE VM on LabHost PC."
+ECHO Ejecting media and creating snapshot of CPBASE VM on LabHost PC...
 PATH|find /i "Oracle\VirtualBox" >nul || set path=%PATH%;C:\Program Files\Oracle\VirtualBox
 SET VM=CPBASE
 VBoxManage storageattach %VM% --storagectl "IDE" --port 0 --device 0 --medium emptydrive
 VBoxManage snapshot %VM% take "Snapshot 1" --description "Do NOT delete!!! This snapshot is used to spawn the linked clones of all Check Point components in the lab."
-
