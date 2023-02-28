bc6dc6c709aa4bea785e24ae6e3327e410dd4394
higherintelligence <92738512+higherintelligence@users.noreply.github.com>
2023-02-28 15:59:43 -0500

    Update create_CPBASE.bat

--- Chapter04/Scripts/create_CPBASE.bat
+++ Chapter04/Scripts/create_CPBASE.bat
@@ -1,2 +1,3 @@
+@ECHO OFF
 powershell write-host -fore Red !!!create_CPBASE.bat is now deprecated!!! 
-powershell write-host -fore Cyan 'Instead create_CPBASE.bat, execute one of the create_CPBASE_CPU.bat files for either Intel or AMD CPUs in LabHost PC.'
+powershell write-host -fore Cyan 'Instead, execute one of the create_CPBASE_CPU.bat files for either Intel or AMD CPUs in LabHost PC.'
